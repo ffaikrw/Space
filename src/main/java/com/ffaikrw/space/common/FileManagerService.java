6e8616e86ab06a7ed21b1926a0425201796c6db7
303-10\megait <asdf@gmail.com>
2022-05-10 16:31:38 +0900

    [정혜원] - aws 기본 설정 추가

--- src/main/java/com/ffaikrw/space/common/FileManagerService.java
+++ src/main/java/com/ffaikrw/space/common/FileManagerService.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class FileManagerService {
 
-	public final static String FILE_UPLOAD_PATH = "D:\\정혜원\\SpringProject\\Space\\upload\\images/";
+	public final static String FILE_UPLOAD_PATH = "/home/ec2-user/space/upload/images/";
 	
 	private static Logger logger = LoggerFactory.getLogger(FileManagerService.class);
 	
